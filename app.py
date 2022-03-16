6d5ad6d8412d7380e755dbc3f25a819a06cb161e
Fırat Yılmaz <43973358+firatylmz06@users.noreply.github.com>
2022-03-16 12:41:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, jsonify, request
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 from bson.json_util import dumps
 from bson.objectid import ObjectId
